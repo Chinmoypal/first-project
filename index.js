b84b93b3a24c8d64965a4eee860461389ad83c0b
Chinmopal <chinmoy.pal2001@gmail.com>
2024-03-25 19:16:42 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express'); 
+const app = express();
+const port = 5000;
+app.set('view engine','ejs')
+app.get('/',(req,res)=> {
+    res.send("HELLO BONGUT")
+})
+app.listen(port, ()=>{
+    console.log("server is live on port",port);
+})
